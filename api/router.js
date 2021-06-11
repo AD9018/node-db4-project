0cf8fec7366c7646581202d513d8de04bc5b7710
AD9018 <adamdadi23@gmail.com>
2021-06-11 02:55:35 -0400

    skeleton built

---
+++ api/router.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const Recipe = require('./model')
+
+const router = express.Router()
+
+router.get('/:recipe_id',(req, res,next) => {
+
+})
+
+module.exports = router
